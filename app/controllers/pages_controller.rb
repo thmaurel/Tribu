3f5fbda9ef0ff26e1f04d111149d06faae601d2a
Thibaud Maurel <maurel.thibaud@free.fr>
2022-10-01 03:07:18 +0200

    display all cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
+  SIDES = ["aubergine", "jambon", "olive", "tomate", "ananas", "champi", "chevre", "salade", "white"]
   def home
+    session[:round] = 1
+    @tuiles = []
+    6.times do 
+      tuile = []
+      3.times {tuile << SIDES.sample}
+      @tuiles << tuile
+    end
   end
 end
