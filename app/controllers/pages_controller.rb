040e56c6218ad57484705c383b2bf60196d038a5
Thibaud Maurel <maurel.thibaud@free.fr>
2022-10-04 13:14:14 +0200

    attaque is dynamic based on ingredients repartition

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,16 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
 
   SIDES = ["aubergine", "jambon", "olive", "tomate", "ananas", "champi", "chevre", "salade", "white"]
+
   def home
     session[:round] = 1
     @tuiles = []
+    @attaques = []
     6.times do
       tuile = []
       3.times { tuile << SIDES.sample }
       @tuiles << tuile
+      @attaques << compute_attaque(tuile)
     end
     @player = Player.new
     @player = Player.find(params[:player]) if params[:player]
@@ -37,17 +40,39 @@ class PagesController < ApplicationController
 
   def pioche
     tuile = [SIDES.sample, SIDES.sample, SIDES.sample]
+    attaque = compute_attaque(tuile)
     respond_to do |format|
       format.html # Follow regular flow of Rails
-      format.text { render partial: "tuile", locals: { tuile:, attaque: rand(0..2), miror: "" }, formats: [:html] }
+      format.text { render partial: "tuile", locals: { tuile:, attaque:, miror: "" }, formats: [:html] }
     end
   end
 
   def piocheup
     tuile = [SIDES.sample, SIDES.sample, SIDES.sample]
+    attaque = [compute_attaque(tuile) + 2, 3].max
     respond_to do |format|
       format.html # Follow regular flow of Rails
-      format.text { render partial: "tuile2", locals: { tuile:, attaque: rand(2..4), miror: "" }, formats: [:html] }
+      format.text { render partial: "tuile2", locals: { tuile:, attaque:, miror: "" }, formats: [:html] }
+    end
+  end
+
+  private
+
+  def compute_attaque(tuile)
+    if tuile.uniq.reject{ |c| c == "white" }.length == 3
+      rand(0..2).zero? ? 1 : 0
+    elsif tuile.uniq.length == 2 && !tuile.include?("white")
+      rand(0..2).zero? ? 0 : 1
+    elsif tuile.uniq.length == 3
+      rand(0..2).zero? ? 2 : 1
+    elsif tuile.uniq.length < 3 && tuile.count { |t| t == "white" } < 2
+      rand(0..2).zero? ? 1 : 2
+    elsif tuile.uniq.length == 2 && tuile.count { |t| t == "white" } == 2
+      rand(0..2).zero? ? 3 : 2
+    elsif tuile.uniq == ["white"]
+      3
+    else
+      10
     end
   end
 end
